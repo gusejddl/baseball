5263ef33630180431d24d9f5acc910955a3e8058
hyunjung <gusejddl519@naver.com>
2024-04-29 10:41:00 +0900

    실습(1/8)

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,2 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class GameTest {
+
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
+
+    @Test
+    void createGame(){
+        assertNotNull(game);
+    }
+
+    @Test
+    void throwExceptionWhenInputIsNull(){
+        assertThrows(IllegalAccessException.class,()->{
+            game.guess(null);
+        });
+    }
 }
+
