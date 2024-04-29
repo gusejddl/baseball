785803057a3f2ebb43fdaaa3d9137099225d8ba0
hyunjung <gusejddl519@naver.com>
2024-04-29 12:39:18 +0900

    실습(8/8)

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -4,18 +4,34 @@ public class Game {
     public String question;
     public GuessResult guess(String guessNumber) {
         assertIllegalArgument(guessNumber);
-        if(guessNumber.equals(question)){
-            return new GuessResult(true,3,0);
+        if(isSolved(guessNumber)){
+            return getGuessResult();
         }
         else {
             int strikes =0;
+            int balls =0;
             for (int i=0;i<question.length();i++){
                 if(question.indexOf(guessNumber.charAt(i)) ==i){
                     strikes++;
                 }
+                else if(question.indexOf(guessNumber.charAt(i))>-1){
+                    balls++;
+                }
             }
-            return new GuessResult(false,strikes,0);
+            return createUnSolvedResult(strikes, balls);
         }
+    }
+
+    private static GuessResult createUnSolvedResult(int strikes, int balls) {
+        return new GuessResult(false, strikes, balls);
+    }
+
+    private static GuessResult getGuessResult() {
+        return new GuessResult(true, 3, 0);
+    }
+
+    private boolean isSolved(String guessNumber) {
+        return guessNumber.equals(question);
     }
 
     private static void assertIllegalArgument(String guessNumber) {
