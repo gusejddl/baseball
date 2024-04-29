402afcafb2b8c2c6b1024112893690c25cd4b218
hyunjung <gusejddl519@naver.com>
2024-04-29 10:59:37 +0900

    실습(4/8)

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,19 +1,32 @@
 public class Game {
-    public void guess(String guessNumber)  {
-        if(guessNumber == null){
+    public void guess(String guessNumber) {
+        assertIllegalArgument(guessNumber);
+
+    }
+
+    private static void assertIllegalArgument(String guessNumber) {
+        if (guessNumber == null) {
             throw new IllegalArgumentException();
         }
 
-        if(guessNumber.length()!=3){
+        if (guessNumber.length() != 3) {
             throw new IllegalArgumentException();
         }
 
-        for(char number :  guessNumber.toCharArray()){
-            if(number < '0' || number>'9'){
-                throw  new IllegalArgumentException();
+        for (char number : guessNumber.toCharArray()) {
+            if (number < '0' || number > '9') {
+                throw new IllegalArgumentException();
             }
-
         }
 
+        if (isDuplicatedNumber(guessNumber)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean isDuplicatedNumber(String guessNumber) {
+        return guessNumber.charAt(0) == guessNumber.charAt(1)
+                || guessNumber.charAt(0) == guessNumber.charAt(2)
+                || guessNumber.charAt(1) == guessNumber.charAt(2);
     }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -32,8 +32,8 @@ public class GameTest {
         assertIllegalArgument(null);
         assertIllegalArgument("12");
         assertIllegalArgument("1234");
-
         assertIllegalArgument("12S");
+        assertIllegalArgument("121");
     }
 
 
